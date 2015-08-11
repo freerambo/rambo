dd917cc7b010a3497973962ab6fb447c68fecfc7
zhuyb <zhuyb@ntu.edu.sg>
2015-08-11 22:04:33 +0800

    update the test null object

--- Yuanbo/src/NullTest.java
+++ Yuanbo/src/NullTest.java
@@ -2,7 +2,14 @@ public class NullTest {
     public static void main(String[] args) {
         testNullObject();
     }
-    
+    /**
+     * 
+     *   
+     * @description    testNullObject
+     * @version currentVersion  
+     * @author Yuanbo Zhu  
+     * @createtime Aug 11, 2015 10:03:45 PM
+     */
     public static void testNullObject() {
         if (null instanceof java.lang.Object) {
             System.out.println("null Ù”⁄java.lang.Object¿‡–Õ");
