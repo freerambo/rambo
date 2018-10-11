a9754e12499465c4010aa7665ca8c27c41e0ffa4
Zhu, Yuanbo <yuzhu@visa.com>
2018-10-11 22:05:47 +0800

    add base64

--- Yuanbo/src/v/BigIntTest.java
+++ Yuanbo/src/v/BigIntTest.java
@@ -6,9 +6,13 @@ public class BigIntTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = (long)Math.pow(2, 64) + "";
-		BigInteger bi = new BigInteger(s);
-		System.out.println(s+"\n"+bi);
+		String s1 = (long)Math.pow(2, 64) + "";
+		BigInteger bi = getBigInteger(s1);
+		System.out.println(s1+"\n"+bi);
+	}
+
+	private static BigInteger getBigInteger(String s) {
+		return new BigInteger(s);
 	}
 
 }
