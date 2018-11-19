e67918c7afc53ca26e32e22d2a71406428f37256
Zhu, Yuanbo <yuzhu@visa.com>
2018-11-19 23:08:32 +0800

    add an example with String joiner

---
+++ rambo/practice/src/com/rambo/StingJoinerTest.java
@@ -0,0 +1,17 @@
+package com.rambo;
+
+import java.util.StringJoiner;
+
+/**
+ * Created by yuzhu on 19/11/18.
+ */
+public class StingJoinerTest {
+
+    public static void main(String[] args){
+
+        StringJoiner sj = new StringJoiner("-");
+        sj.add("hello").add("world");
+        System.out.println(sj.toString());
+
+    }
+}
